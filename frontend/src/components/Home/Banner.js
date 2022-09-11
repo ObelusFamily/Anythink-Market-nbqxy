f9e922d3d1de204113b2adc02ed8348f89c856a4
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-09-11 14:37:25 +0530

    [Ashutosh] Add. search bar

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,15 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import SearchBar from "./SearchBar";
 
-const Banner = () => {
+const Banner = ({ title, setTitle }) => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
+          <span id="get-part">A place to get </span>
+          <SearchBar title={title} setTitle={setTitle} />
           <span> the cool stuff.</span>
         </div>
       </div>
